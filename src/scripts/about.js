affd332981ad1a1be40b47d967301d591ff7ce60
che.lin <aa10327tw@gmail.com>
2020-07-14 12:33:09 +0800

    git init

---
+++ src/scripts/about.js
@@ -0,0 +1,5 @@
+const about = () => console.log('about');
+const about1 = () => console.log('about1');
+
+about();
+about1();
---
+++ src/scripts/contact.js
@@ -0,0 +1,3 @@
+const contact = () => console.log('contact');
+
+contact();
