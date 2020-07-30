33a8b01aeed48694468b565aa0d879efcff29155
che.lin <aa10327tw@gmail.com>
2020-07-30 12:49:05 +0800

    新增 gulp-ejs

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,7 @@
+// built-in
+import fs from 'fs';
+import path from 'path';
+// plugins
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import nodeSass from 'node-sass';
@@ -13,8 +17,8 @@ const envOptions = {
   string: 'env',
   // 預設會輸出 develop 字串
   default: {
-    env: 'develop',
-  },
+    env: 'develop'
+  }
 };
 
 const options = minimist(process.argv.slice(2), envOptions);
@@ -23,21 +27,24 @@ console.log(`Current mode：${options.env}`);
 
 const paths = {
   json: {
-    menuSrc: './src/data/menu.json',
-    indexSrc: './src/data/index.json',
+    src: './src/data/'
+  },
+  ejs: {
+    src: ['./src/templates/**/*.ejs', '!./src/templates/**/_*.ejs'],
+    dest: 'dist/'
   },
   html: {
     src: './src/**/*.html',
     ejsSrc: './src/**/*.ejs',
-    dest: 'dist/',
+    dest: 'dist/'
   },
   styles: {
     src: './src/scss/**/*.scss',
-    dest: 'dist/styles/',
+    dest: 'dist/styles/'
   },
   scripts: {
     src: './src/scripts/**/*.js',
-    dest: 'dist/scripts/',
+    dest: 'dist/scripts/'
   },
   images: {
     src: [
@@ -45,10 +52,10 @@ const paths = {
       './src/images/**/*.jpeg',
       './src/images/**/*.png',
       './src/images/**/*.gif',
-      './src/images/**/*.svg',
+      './src/images/**/*.svg'
     ],
-    dest: 'dist/images/',
-  },
+    dest: 'dist/images/'
+  }
 };
 
 $.sass.compiler = nodeSass;
@@ -57,41 +64,44 @@ export const clean = () => del(['dist']);
 
 export function ejs() {
   return gulp
-    .src([paths.html.ejsSrc, paths.html.src])
+    .src(paths.ejs.src)
     .pipe(
       $.data(function (file) {
-        const menu = require(paths.json.menuSrc);
-        const indexContent = require(paths.json.indexSrc);
-        return { menu, indexContent };
+        const { path: filePath } = file;
+        // global.json 全域資料，頁面中直接通過屬性名調用
+        return Object.assign(
+          JSON.parse(fs.readFileSync(paths.json.src + 'global.json')),
+          {
+            // local: 每個頁面對應的資料，頁面中通過 local.屬性調用
+            local: JSON.parse(
+              fs.readFileSync(
+                path.join(
+                  paths.json.src + path.basename(filePath, '.ejs') + '.json'
+                )
+              )
+            )
+          }
+        );
       })
     )
     .pipe($.ejs())
-    .pipe(gulp.dest(paths.html.dest));
+    .pipe($.rename({ extname: '.html' }))
+    .pipe(gulp.dest(paths.ejs.dest))
+    .pipe(browserSync.stream());
 }
 
 export function layoutHTML() {
-  return (
-    gulp
-      .src([paths.html.src, paths.html.ejsSrc])
-      .pipe($.plumber())
-      .pipe(
-        $.data(function (file) {
-          const menu = require(paths.json.menuSrc);
-          const indexContent = require(paths.json.indexSrc);
-          const title = '首頁';
-          return { menu, indexContent, title };
-        })
-      )
-      .pipe($.ejs())
-      // .pipe($.frontMatter())
-      // .pipe(
-      //   $.layout((file) => {
-      //     return file.frontMatter;
-      //   })
-      // )
-      .pipe(gulp.dest(paths.html.dest))
-      .pipe(browserSync.stream())
-  );
+  return gulp
+    .src(paths.html.src)
+    .pipe($.plumber())
+    .pipe($.frontMatter())
+    .pipe(
+      $.layout((file) => {
+        return file.frontMatter;
+      })
+    )
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream());
 }
 
 export function styles() {
@@ -112,30 +122,32 @@ export function styles() {
 }
 
 export function scripts() {
-  return gulp
-    .src(paths.scripts.src, { sourcemaps: true })
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe(
-      $.babel({
-        presets: ['@babel/env'],
-      })
-    )
-    .pipe(
-      $.if(
-        options.env === 'production',
-        $.uglify({
-          compress: {
-            drop_console: true,
-          },
+  return (
+    gulp
+      .src(paths.scripts.src, { sourcemaps: true })
+      .pipe($.plumber())
+      .pipe($.sourcemaps.init())
+      .pipe(
+        $.babel({
+          presets: ['@babel/env']
         })
       )
-    )
-    .pipe($.order(['index.js', 'about.js', 'contact.js']))
-    .pipe($.concat('all.js'))
-    .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.scripts.dest))
-    .pipe(browserSync.stream());
+      .pipe(
+        $.if(
+          options.env === 'production',
+          $.uglify({
+            compress: {
+              drop_console: true
+            }
+          })
+        )
+      )
+      // .pipe($.order(['index.js', 'about.js', 'contact.js']))
+      // .pipe($.concat('all.js'))
+      .pipe($.sourcemaps.write('./'))
+      .pipe(gulp.dest(paths.scripts.dest))
+      .pipe(browserSync.stream())
+  );
 }
 
 export function images() {
@@ -148,13 +160,15 @@ export function images() {
 export function browser() {
   browserSync.init({
     server: {
-      baseDir: './dist/',
+      baseDir: './dist/'
     },
-    port: 8082,
+    port: 8082
   });
 }
 
 export function watchFiles() {
+  gulp.watch(`${paths.json.src}**/*.json`, ejs);
+  gulp.watch(paths.ejs.src, ejs);
   gulp.watch(paths.html.src, layoutHTML);
   gulp.watch(paths.html.ejsSrc, layoutHTML);
   gulp.watch(paths.scripts.src, scripts);
@@ -168,12 +182,20 @@ export function deploy() {
 const dev = gulp.series(
   clean,
   images,
+  ejs,
   layoutHTML,
   styles,
   scripts,
   gulp.parallel(browser, watchFiles)
 );
 
-export const build = gulp.series(clean, images, layoutHTML, styles, scripts);
+export const build = gulp.series(
+  clean,
+  images,
+  ejs,
+  layoutHTML,
+  styles,
+  scripts
+);
 
 export default dev;
